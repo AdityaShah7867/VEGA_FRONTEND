ccd0c35980f7871e56abe0d9402d8ab64be0f000
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-03-06 16:52:54 +0530

    feedback done

--- src/components/Calender/AdvancedCalender.jsx
+++ src/components/Calender/AdvancedCalender.jsx
@@ -1,48 +1,36 @@
 import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import Calendar from "./Calender";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-const events = [
-  {
-    start: moment("2023-03-18T10:00:00").toDate(),
-    end: moment("2023-03-18T11:00:00").toDate(),
-    title: "MRI Registration",
-    data: {
-      type: "Reg",
-    },
-  },
-  {
-    start: moment("2023-03-18T14:00:00").toDate(),
-    end: moment("2023-03-18T15:30:00").toDate(),
-    title: "ENT Appointment",
-    data: {
-      type: "App",
-    },
-  },
-];
+const MyCalendar = () => {
+  const localizer = momentLocalizer(moment);
 
-const components = {
-  event: (props) => {
-    const eventType = props?.event?.data?.type;
-    switch (eventType) {
-      case "Reg":
-        return (
-          <div style={{ background: "yellow", color: "white", height: "100%" }}>
-            {props.title}
-          </div>
-        );
-      case "App":
-        return (
-          <div style={{ background: "lightgreen", color: "white", height: "100%" }}>
-            {props.title}
-          </div>
-        );
-      default:
-        return null;
-    }
-  },
+
+  const events = [
+    {
+      title: "Event 1",
+      start: new Date(2024, 2, 6, 10, 0),
+      end: new Date(2024, 2, 6, 12, 0),
+    },
+    {
+      title: "Event 2",
+      start: new Date(2024, 2, 7, 14, 0),
+      end: new Date(2024, 2, 7, 16, 0),
+    },
+  ];
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+      />
+    </div>
+  );
 };
 
-export default function ControlCalendar() {
-  return <Calendar events={events} components={components} />;
-}
+export default MyCalendar;
